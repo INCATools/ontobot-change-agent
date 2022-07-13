61f97b36020ab1d20cc3430522b7d7eb1f570a0f
Harshad Hegde <hegdehb@gmail.com>
2022-07-13 11:05:22 -0500

    cleaned up tests

--- tests/test_api.py
+++ tests/test_api.py
@@ -3,23 +3,28 @@
 """Test all API functions."""
 
 import unittest
-from onto_crawler.api import TOKEN, get_issues
+
 from github.Issue import Issue
+
+from onto_crawler.api import TOKEN, get_issues
+
 
 class TestAPI(unittest.TestCase):
     """Test all API functions."""
 
     def setUp(self) -> None:
-        """Set up variables"""
+        """Set up variables."""
         self.repo_name = "hrshdhgd/onto-crawler"
         self.token = TOKEN
         self.label = "test"
         self.issue_title = "Test issue"
 
     def test_get_issues_with_label(self):
-        """Test if 'get_issues' returns the correct label"""
+        """Test if 'get_issues' returns the correct label."""
         issues = []
-        for issue in get_issues(repository_name=self.repo_name, label=self.label):
+        for issue in get_issues(
+            repository_name=self.repo_name, label=self.label
+        ):
             issues.append(issue)
 
         self.assertEqual(len(issues), 1)
@@ -29,9 +34,11 @@ class TestAPI(unittest.TestCase):
         self.assertTrue(issues[0].labels[0].name, self.label)
 
     def test_get_issues_with_title(self):
-        """Test if 'get_issues' returns the correct label"""
+        """Test if 'get_issues' returns the correct title."""
         issues = []
-        for issue in get_issues(repository_name=self.repo_name, title_search=self.issue_title):
+        for issue in get_issues(
+            repository_name=self.repo_name, title_search=self.issue_title
+        ):
             issues.append(issue)
 
         self.assertEqual(len(issues), 1)
@@ -39,6 +46,3 @@ class TestAPI(unittest.TestCase):
         self.assertTrue(issues[0].number, 2)
         self.assertTrue(issues[0].title, self.issue_title)
         self.assertTrue(issues[0].labels[0].name, self.label)
-
-    
-
--- tests/test_cli.py
+++ tests/test_cli.py
@@ -3,9 +3,12 @@
 """Test all CLI commands."""
 
 import unittest
+
 from click.testing import CliRunner
+
 from onto_crawler.api import TOKEN
 from onto_crawler.cli import issues
+
 
 class TestVersion(unittest.TestCase):
     """Test all CLI commands."""
@@ -25,4 +28,3 @@ class TestVersion(unittest.TestCase):
         result.stdout
         result.stderr
         self.assertEqual(0, result.exit_code)
-
