61fcce2857462b28e1c0a3c91f1aad2151324bab
Harshad Hegde <hegdehb@gmail.com>
2022-12-08 16:44:51 -0600

    echo without format

--- src/ontobot_change_agent/api.py
+++ src/ontobot_change_agent/api.py
@@ -34,7 +34,7 @@ TOKEN_FILE = join(SRC, "token.txt")
 
 if os.getenv("GITHUB_ENV"):
     click.echo(
-        f"'${{ secrets.GH_TOKEN }}' >> {TOKEN_FILE}"
+        "${{ secrets.GH_TOKEN }} >> " + TOKEN_FILE
     )
 
 with open(TOKEN_FILE, "r") as t:
